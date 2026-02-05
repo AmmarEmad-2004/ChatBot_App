415a6212b4bb19279cb375d67616ae5f41f74769
Ammar Emad <ammarzewain99@gmail.com>
2026-02-05 20:47:14 +0200

    Add AppImages class and update pubspec.yaml for asset paths

---
+++ lib/core/constants/AppImages.dart
@@ -0,0 +1,32 @@
+// ignore_for_file: prefer_single_quotes
+class AppImages {
+  AppImages._();
+
+  /// Assets for assetsImagesBlueRobot
+  /// assets/images/blue-robot.svg
+  static const String imagesBlueRobot = "assets/images/blue-robot.svg";
+
+  /// Assets for assetsImagesEdit2
+  /// assets/images/edit-2.svg
+  static const String imagesEdit2 = "assets/images/edit-2.svg";
+
+  /// Assets for assetsImagesExplain
+  /// assets/images/explain.svg
+  static const String imagesExplain = "assets/images/explain.svg";
+
+  /// Assets for assetsImagesOnboarding
+  /// assets/images/onboarding.svg
+  static const String imagesOnboarding = "assets/images/onboarding.svg";
+
+  /// Assets for assetsImagesRobot
+  /// assets/images/robot.svg
+  static const String imagesRobot = "assets/images/robot.svg";
+
+  /// Assets for assetsImagesSplash
+  /// assets/images/splash.svg
+  static const String imagesSplash = "assets/images/splash.svg";
+
+  /// Assets for assetsImagesTranslate
+  /// assets/images/translate.svg
+  static const String imagesTranslate = "assets/images/translate.svg";
+}
