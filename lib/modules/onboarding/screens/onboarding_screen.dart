1c1043bc87fef26ae69a9a19883c50afcfa780ec
Ammar Emad <ammarzewain99@gmail.com>
2026-02-06 01:53:20 +0200

    feat: move CustomButton to its own file for better modularity

--- lib/modules/onboarding/screens/onboarding_screen.dart
+++ lib/modules/onboarding/screens/onboarding_screen.dart
@@ -1,7 +1,7 @@
 import 'package:chatbot_app/core/constants/app_images.dart';
 import 'package:chatbot_app/core/constants/constants.dart';
-import 'package:chatbot_app/core/theme/app_colors.dart';
 import 'package:chatbot_app/core/theme/text_styles.dart';
+import 'package:chatbot_app/modules/onboarding/widgets/custom_button.dart';
 import 'package:flutter/material.dart';
 
 class OnboardingScreen extends StatelessWidget {
@@ -39,33 +39,3 @@ class OnboardingScreen extends StatelessWidget {
   }
 }
 
-class CustomButton extends StatelessWidget {
-  const CustomButton({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 56,
-      width: double.infinity,
-      decoration: BoxDecoration(
-        color: AppColors.primaryColor,
-        borderRadius: BorderRadius.circular(30),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            Expanded(
-              child: Center(
-                child: Text('Continue', style: TextStyles.textBold19(context)),
-              ),
-            ),
-
-            Icon(Icons.arrow_forward_outlined, color: Colors.white, size: 24),
-          ],
-        ),
-      ),
-    );
-  }
-}
