ad0995cb97458af63bcd0a26d2a120d7bb02d460
Ammar Emad <ammarzewain99@gmail.com>
2026-02-05 21:04:43 +0200

    create splash screen

--- lib/chat_bot.dart
+++ lib/chat_bot.dart
@@ -1,3 +1,4 @@
+import 'package:chatbot_app/modules/splash/screens/splash_screen.dart';
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 
@@ -7,17 +8,12 @@ class ChatBot extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-    
+      debugShowCheckedModeBanner: false,
       locale: DevicePreview.locale(context),
       builder: DevicePreview.appBuilder,
-      home: Scaffold(
-        body: Center(
-          child: Text(
-            'Hello, I am your ChatBot!',
-            style: TextStyle(fontSize: 24),
-          ),
-        ),
-      ),
+      home: SplashScreen(),
     );
   }
 }
+
+
