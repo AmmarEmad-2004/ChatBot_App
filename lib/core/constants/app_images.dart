7cdefe467897dd8feb3de50cc11df6775496c400
Ammar Emad <ammarzewain99@gmail.com>
2026-02-06 02:52:09 +0200

    feat: add new image assets and update AppImages constants for better resource management

--- lib/core/constants/app_images.dart
+++ lib/core/constants/app_images.dart
@@ -4,37 +4,53 @@ class AppImages {
 
   /// Assets for assetsImagesBlueRobot
   /// assets/images/blue-robot.svg
-  static const String imagesBlueRobot = "assets/images/blue-robot.svg";
+  static const String blueRobot = "assets/images/blue-robot.svg";
+
+  /// Assets for assetsImagesBlueRobot2
+  /// assets/images/blue-robot2.png
+  static const String blueRobot2 = "assets/images/blue-robot2.png";
 
   /// Assets for assetsImagesEdit2
   /// assets/images/edit-2.svg
-  static const String imagesEdit2 = "assets/images/edit-2.svg";
+  static const String edit2 = "assets/images/edit-2.svg";
 
   /// Assets for assetsImagesExplain
   /// assets/images/explain.svg
-  static const String imagesExplain = "assets/images/explain.svg";
+  static const String explain = "assets/images/explain.svg";
+
+  /// Assets for assetsImagesExport
+  /// assets/images/export.svg
+  static const String export = "assets/images/export.svg";
 
   /// Assets for assetsImagesOnboarding
   /// assets/images/onboarding.svg
-  static const String imagesOnboarding = "assets/images/onboarding.svg";
+  static const String onboarding = "assets/images/onboarding.svg";
 
-  /// Assets for assetsImagesOnboarding
-  /// assets/images/onboarding.svg
-  static const String imagesOnboarding2 = "assets/images/onboarding2.png";
+  /// Assets for assetsImagesOnboarding2
+  /// assets/images/onboarding2.png
+  static const String onboarding2 = "assets/images/onboarding2.png";
 
   /// Assets for assetsImagesRobot
   /// assets/images/robot.svg
-  static const String imagesRobot = "assets/images/robot.svg";
+  static const String robot = "assets/images/robot.svg";
+
+  /// Assets for assetsImagesRobot2
+  /// assets/images/robot2.png
+  static const String robot2 = "assets/images/robot2.png";
 
   /// Assets for assetsImagesSplash
   /// assets/images/splash.svg
-  static const String imagesSplash = "assets/images/splash.svg";
+  static const String splash = "assets/images/splash.svg";
 
   /// Assets for assetsImagesSplash2
   /// assets/images/splash2.png
-  static const String imagesSplash2 = "assets/images/splash2.png";
+  static const String splash2 = "assets/images/splash2.png";
 
   /// Assets for assetsImagesTranslate
   /// assets/images/translate.svg
-  static const String imagesTranslate = "assets/images/translate.svg";
+  static const String translate = "assets/images/translate.svg";
+
+  /// Assets for assetsImagesVolumeHigh
+  /// assets/images/volume-high.svg
+  static const String volumeHigh = "assets/images/volume-high.svg";
 }
