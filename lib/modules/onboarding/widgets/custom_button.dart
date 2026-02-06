cc98d9665bbbcb460aa8b78a18da7d65ca82bfdc
Ammar Emad <ammarzewain99@gmail.com>
2026-02-06 16:24:14 +0200

    feat: enhance CustomButton with navigation to chat screen on tap

--- lib/modules/onboarding/widgets/custom_button.dart
+++ lib/modules/onboarding/widgets/custom_button.dart
@@ -1,33 +1,42 @@
-
+import 'package:chatbot_app/core/routing/app_routers.dart';
 import 'package:chatbot_app/core/theme/app_colors.dart';
 import 'package:chatbot_app/core/theme/text_styles.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class CustomButton extends StatelessWidget {
   const CustomButton({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 56,
-      width: double.infinity,
-      decoration: BoxDecoration(
-        color: AppColors.primaryColor,
-        borderRadius: BorderRadius.circular(30),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            Expanded(
-              child: Center(
-                child: Text('Continue', style: TextStyles.textBold19(context)),
+    return InkWell(
+      onTap: () {
+        GoRouter.of(context).go(AppRouters.chatScreen);
+      },
+      child: Container(
+        height: 56,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: AppColors.primaryColor,
+          borderRadius: BorderRadius.circular(30),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Expanded(
+                child: Center(
+                  child: Text(
+                    'Continue',
+                    style: TextStyles.textBold19(context),
+                  ),
+                ),
               ),
-            ),
 
-            Icon(Icons.arrow_forward_outlined, color: Colors.white, size: 24),
-          ],
+              Icon(Icons.arrow_forward_outlined, color: Colors.white, size: 24),
+            ],
+          ),
         ),
       ),
     );
