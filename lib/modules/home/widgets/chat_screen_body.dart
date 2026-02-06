e87f07281ccdbc5a59276d2abafdcb209815f30a
Ammar Emad <ammarzewain99@gmail.com>
2026-02-06 16:23:57 +0200

    feat: implement chat message display in ChatScreenBody

--- lib/modules/home/widgets/chat_screen_body.dart
+++ lib/modules/home/widgets/chat_screen_body.dart
@@ -1,3 +1,4 @@
+import 'package:chatbot_app/modules/home/widgets/chat_bubble.dart';
 import 'package:flutter/material.dart';
 
 class ChatScreenBody extends StatelessWidget {
@@ -5,6 +6,21 @@ class ChatScreenBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("Home Screen"));
+    final messages = [
+      {"text": "Hello chatGPT, how are you today?", "isMe": true},
+      {"text": "Hello, I'm fine. How can I help you?", "isMe": false},
+      {"text": "What is the best programming language?", "isMe": true},
+      {"text": "There are many programming languages...", "isMe": false},
+    ];
+    return ListView.builder(
+      padding: const EdgeInsets.all(8),
+      itemCount: messages.length,
+      itemBuilder: (context, index) {
+        return ChatBubble(
+          text: messages[index]["text"]! as String,
+          isMe: messages[index]["isMe"]! as bool,
+        );
+      },
+    );
   }
 }
