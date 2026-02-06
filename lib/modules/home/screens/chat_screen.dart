cc25abb95a5df30cca340cfb91b6ff50a05e1891
Ammar Emad <ammarzewain99@gmail.com>
2026-02-06 03:17:40 +0200

    feat: add ChatScreenBody widget and update ChatScreen layout

--- lib/modules/home/screens/chat_screen.dart
+++ lib/modules/home/screens/chat_screen.dart
@@ -1,4 +1,5 @@
 import 'package:chatbot_app/modules/home/widgets/chat_screen_header.dart';
+import 'package:chatbot_app/modules/home/widgets/chat_screen_welcome.dart';
 import 'package:flutter/material.dart';
 
 class ChatScreen extends StatelessWidget {
@@ -7,6 +8,7 @@ class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: Column(
         children: [
           ChatScreenHeader(),
@@ -16,31 +18,6 @@ class ChatScreen extends StatelessWidget {
           ChatScreenWelcome(),
         ],
       ),
-    );
-  }
-}
-
-class ChatScreenBody extends StatelessWidget {
-  const ChatScreenBody({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(child: Text("Home Screen"));
-  }
-}
-
-class ChatScreenWelcome extends StatelessWidget {
-  const ChatScreenWelcome({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Text("Welcome to ChatBot App"),
-        SizedBox(height: 16),
-        Text("Your AI Assistant"),
-      ],
     );
   }
 }
