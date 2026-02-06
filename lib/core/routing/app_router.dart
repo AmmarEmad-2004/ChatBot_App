db852de6ff723071a7ebcf150a0d23d32b0e9e5e
Ammar Emad <ammarzewain99@gmail.com>
2026-02-06 13:58:00 +0200

    feat: add AppRouter for navigation and update dependencies for go_router

---
+++ lib/core/routing/app_router.dart
@@ -0,0 +1,25 @@
+import 'package:chatbot_app/core/routing/app_routers.dart';
+import 'package:chatbot_app/modules/home/screens/chat_screen.dart';
+import 'package:chatbot_app/modules/onboarding/screens/onboarding_screen.dart';
+import 'package:chatbot_app/modules/splash/screens/splash_screen.dart';
+import 'package:go_router/go_router.dart';
+
+abstract class AppRouter {
+  static final goRouter = GoRouter(
+    routes: [
+      GoRoute(
+        path: AppRouters.splash,
+        builder: (context, state) => const SplashScreen(),
+      ),
+      GoRoute(
+        path: AppRouters.onboarding,
+        builder: (context, state) => const OnboardingScreen(),
+      ),
+      GoRoute(
+        path: AppRouters.chatScreen,
+        builder: (context, state) => const ChatScreen(),
+      ),
+     
+    ],
+  );
+}
