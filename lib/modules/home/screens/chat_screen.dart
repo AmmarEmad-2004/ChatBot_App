9058d5fb4dc06b5ca65d288797936027cb5a39ef
Ammar Emad <ammarzewain99@gmail.com>
2026-02-06 16:23:37 +0200

    feat: replace ChatScreenWelcome with ChatScreenBody in ChatScreen layout

--- lib/modules/home/screens/chat_screen.dart
+++ lib/modules/home/screens/chat_screen.dart
@@ -1,3 +1,4 @@
+import 'package:chatbot_app/modules/home/widgets/chat_screen_body.dart';
 import 'package:chatbot_app/modules/home/widgets/chat_screen_header.dart';
 import 'package:chatbot_app/modules/home/widgets/chat_screen_welcome.dart';
 import 'package:flutter/material.dart';
@@ -15,7 +16,7 @@ class ChatScreen extends StatelessWidget {
           SizedBox(height: 13),
           Divider(color: Color(0xffECECEC)),
           SizedBox(height: 19),
-          ChatScreenWelcome(),
+          Expanded(child: ChatScreenBody()),
         ],
       ),
     );
