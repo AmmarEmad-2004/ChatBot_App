fbfe3d163a1ce75009e1e035a6901b3dc6bcc8df
Ammar Emad <ammarzewain99@gmail.com>
2026-02-07 04:17:18 +0200

    refactor: simplify ChatBot widget structure by removing unnecessary SafeArea

--- lib/chat_bot.dart
+++ lib/chat_bot.dart
@@ -7,13 +7,11 @@ class ChatBot extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: MaterialApp.router(
-        debugShowCheckedModeBanner: false,
-        locale: DevicePreview.locale(context),
-        builder: DevicePreview.appBuilder,
-        routerConfig: AppRouter.goRouter,
-      ),
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
+      routerConfig: AppRouter.goRouter,
     );
   }
 }
