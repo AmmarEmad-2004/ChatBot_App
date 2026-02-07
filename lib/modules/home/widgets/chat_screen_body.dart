11e8ab4a9628148c74aacb0c058a333bf42494d1
Ammar Emad <ammarzewain99@gmail.com>
2026-02-07 04:18:17 +0200

    update chat screen body

--- lib/modules/home/widgets/chat_screen_body.dart
+++ lib/modules/home/widgets/chat_screen_body.dart
@@ -2,23 +2,17 @@ import 'package:chatbot_app/modules/home/widgets/chat_bubble.dart';
 import 'package:flutter/material.dart';
 
 class ChatScreenBody extends StatelessWidget {
-  const ChatScreenBody({super.key});
-
+  const ChatScreenBody({required this.messages, super.key});
+  final List<Map<String, dynamic>> messages;
   @override
   Widget build(BuildContext context) {
-    final messages = [
-      {"text": "Hello chatGPT, how are you today?", "isMe": true},
-      {"text": "Hello, I'm fine. How can I help you?", "isMe": false},
-      {"text": "What is the best programming language?", "isMe": true},
-      {"text": "There are many programming languages...", "isMe": false},
-    ];
     return ListView.builder(
       padding: const EdgeInsets.all(8),
       itemCount: messages.length,
       itemBuilder: (context, index) {
         return ChatBubble(
-          text: messages[index]["text"]! as String,
-          isMe: messages[index]["isMe"]! as bool,
+          text: messages[index]["text"] as String,
+          isMe: messages[index]["isMe"] as bool,
         );
       },
     );
