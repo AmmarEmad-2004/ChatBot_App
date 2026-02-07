53ee5c1e97eafefa49e4b7140ba12f573b864a53
Ammar Emad <ammarzewain99@gmail.com>
2026-02-07 13:26:49 +0200

    feat: Implement the chat screen with message display, input, welcome state, and Cubit logic.

--- lib/modules/home/presentation/screens/chat_screen.dart
+++ lib/modules/home/presentation/screens/chat_screen.dart
@@ -1,67 +1,27 @@
+import 'package:chatbot_app/core/services/gemini_sevice.dart';
+import 'package:chatbot_app/modules/home/data/datasource/chat_local_ds_imple.dart';
+import 'package:chatbot_app/modules/home/data/repo/chat_repo_imple.dart';
+import 'package:chatbot_app/modules/home/presentation/logic/chat_cubit.dart';
 import 'package:chatbot_app/modules/home/presentation/screens/widgets/chat_screen_body.dart';
-import 'package:chatbot_app/modules/home/presentation/screens/widgets/chat_screen_header.dart';
-import 'package:chatbot_app/modules/home/presentation/screens/widgets/chat_screen_welcome.dart';
-import 'package:chatbot_app/modules/home/presentation/screens/widgets/custom_text_feild.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class ChatScreen extends StatefulWidget {
+class ChatScreen extends StatelessWidget {
   const ChatScreen({super.key});
 
   @override
-  State<ChatScreen> createState() => _ChatScreenState();
-}
-
-class _ChatScreenState extends State<ChatScreen> {
-  final TextEditingController controller = TextEditingController();
-  bool showWelcome = true;
-  final List<Map<String, dynamic>> messages = [];
-
-  void sendMessage(String text) {
-    if (text.trim().isEmpty) return;
-
-    setState(() {
-      if (showWelcome) showWelcome = false;
-      messages.add({"text": text, "isMe": true});
-
-      messages.add({"text": "Hello! I am a bot.", "isMe": false});
-    });
-
-    controller.clear();
-  }
-
-  @override
   Widget build(BuildContext context) {
+    final dio = Dio();
+    final geminiDs = GeminiSevice(dio);
+    final localDs = ChatLocalDsImpl(FirebaseFirestore.instance, "default");
+      final chatRepo = ChatRepoImpl(geminiDs, localDs);
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Column(
-        children: [
-          ChatScreenHeader(),
-          SizedBox(height: 13),
-          Divider(color: Color(0xffECECEC)),
-          SizedBox(height: 19),
-          Expanded(
-            child: Stack(
-              children: [
-                Positioned.fill(
-                  child: showWelcome
-                      ? ChatScreenWelcome()
-                      : ChatScreenBody(messages: messages),
-                ),
-                Positioned(
-                  left: 16,
-                  right: 16,
-                  bottom: 16,
-                  child: CustomTextFeild(
-                    controller: controller,
-                    onSend: () {
-                      sendMessage(controller.text);
-                    },
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
+      body: BlocProvider(
+        create: (_) => ChatCubit(chatRepo)..loadChat(),
+        child: ChatScreenBody(),
       ),
     );
   }
--- lib/modules/home/presentation/screens/widgets/chat_bubble.dart
+++ lib/modules/home/presentation/screens/widgets/chat_bubble.dart
@@ -1,16 +1,19 @@
 import 'package:chatbot_app/core/constants/app_images.dart';
 import 'package:chatbot_app/core/theme/app_colors.dart';
 import 'package:chatbot_app/core/theme/text_styles.dart';
+import 'package:chatbot_app/modules/home/data/models/massage_model.dart';
 import 'package:flutter/material.dart';
 
 class ChatBubble extends StatelessWidget {
-  final String text;
-  final bool isMe;
+  final MassageModel message;
 
-  const ChatBubble({required this.text, required this.isMe, super.key});
+  const ChatBubble({required this.message, super.key});
 
   @override
   Widget build(BuildContext context) {
+    final isMe = message.role == "user"; // user or model
+    final text = message.text;
+
     return Align(
       alignment: isMe ? Alignment.centerRight : Alignment.centerLeft,
       child: Padding(
@@ -22,27 +25,30 @@ class ChatBubble extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.end,
           children: [
             if (!isMe) Image.asset(AppImages.robot2),
-            Container(
-              padding: const EdgeInsets.symmetric(vertical: 24, horizontal: 13),
-              margin: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
-              decoration: BoxDecoration(
-                color: isMe ? AppColors.primaryColor : Color(0xffeeeeee),
-                borderRadius: BorderRadius.only(
-                  topLeft: const Radius.circular(30),
-                  topRight: Radius.circular(isMe ? 0 : 30),
-                  bottomLeft: Radius.circular(isMe ? 30 : 0),
-                  bottomRight: Radius.circular(30),
+            Flexible(
+              child: Container(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 24,
+                  horizontal: 13,
                 ),
-              ),
-              child: Text(
-                text,
-                style: isMe
-                    ? TextStyles.textBold13(
-                        context,
-                      ).copyWith(color: Colors.white)
-                    : TextStyles.textBold13(
-                        context,
-                      ).copyWith(color: Color(0xff656565)),
+                margin: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+                decoration: BoxDecoration(
+                  color: isMe
+                      ? AppColors.primaryColor
+                      : const Color(0xffeeeeee),
+                  borderRadius: BorderRadius.only(
+                    topLeft: const Radius.circular(30),
+                    topRight: Radius.circular(isMe ? 0 : 30),
+                    bottomLeft: Radius.circular(isMe ? 30 : 0),
+                    bottomRight: const Radius.circular(30),
+                  ),
+                ),
+                child: Text(
+                  text,
+                  style: TextStyles.textBold13(context).copyWith(
+                    color: isMe ? Colors.white : const Color(0xff656565),
+                  ),
+                ),
               ),
             ),
           ],
