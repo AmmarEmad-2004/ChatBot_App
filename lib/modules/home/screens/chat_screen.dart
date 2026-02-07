21d2bc9f2081bf7fb614607af9ff4e91dbb8f8d7
Ammar Emad <ammarzewain99@gmail.com>
2026-02-07 04:18:51 +0200

    update chat screen and add chat screen body and welcome to it

--- lib/modules/home/screens/chat_screen.dart
+++ lib/modules/home/screens/chat_screen.dart
@@ -1,10 +1,33 @@
 import 'package:chatbot_app/modules/home/widgets/chat_screen_body.dart';
 import 'package:chatbot_app/modules/home/widgets/chat_screen_header.dart';
 import 'package:chatbot_app/modules/home/widgets/chat_screen_welcome.dart';
+import 'package:chatbot_app/modules/home/widgets/custom_text_feild.dart';
 import 'package:flutter/material.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  final TextEditingController controller = TextEditingController();
+  bool showWelcome = true;
+  final List<Map<String, dynamic>> messages = [];
+
+  void sendMessage(String text) {
+    if (text.trim().isEmpty) return;
+
+    setState(() {
+      if (showWelcome) showWelcome = false;
+      messages.add({"text": text, "isMe": true});
+
+      messages.add({"text": "Hello! I am a bot.", "isMe": false});
+    });
+
+    controller.clear();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,7 +39,28 @@ class ChatScreen extends StatelessWidget {
           SizedBox(height: 13),
           Divider(color: Color(0xffECECEC)),
           SizedBox(height: 19),
-          Expanded(child: ChatScreenBody()),
+          Expanded(
+            child: Stack(
+              children: [
+                Positioned.fill(
+                  child: showWelcome
+                      ? ChatScreenWelcome()
+                      : ChatScreenBody(messages: messages),
+                ),
+                Positioned(
+                  left: 16,
+                  right: 16,
+                  bottom: 16,
+                  child: CustomTextFeild(
+                    controller: controller,
+                    onSend: () {
+                      sendMessage(controller.text);
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
