d12af6c79a254259b7a293e6cde56f237d832db4
Ammar Emad <ammarzewain99@gmail.com>
2026-02-13 01:24:11 +0200

    feat: Implement chat repository with local and remote data sources, error handling, and application constants.

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -1,4 +1,5 @@
 const String onboardingText =
     "Using this software,you can ask you\n questions and receive articles using\n artificial intelligence assistant";
-const String apiKey = "AIzaSyCL1c1PmeP9PZPHQMr55jR_p-wv0UHEbKk";
-const String baseUrl = "https://generativelanguage.googleapis.com/v1beta/models/gemini-3-flash-preview:generateContent";
+const String apiKey = "AIzaSyD2WUOeDyxAjHilHXPkG6W3dHV9pYP4OWc";
+const String baseUrl =
+    "https://generativelanguage.googleapis.com/v1beta/models/gemini-3-flash-preview:generateContent";
