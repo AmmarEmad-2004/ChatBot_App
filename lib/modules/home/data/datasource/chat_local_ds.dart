b5d96df7f6b75174910bb1080ac39da3125c7ea3
Ammar Emad <ammarzewain99@gmail.com>
2026-02-13 14:43:45 +0200

    feat: implement chat feature with Gemini API integration and dedicated UI components.

--- lib/modules/home/data/datasource/chat_local_ds.dart
+++
@@ -1,6 +0,0 @@
-import 'package:chatbot_app/modules/home/data/models/massage_model.dart';
-
-abstract class ChatLocalDs {
-  Future<void> saveMessage(MassageModel message);
-  Future<List<MassageModel>> getHistory();
-}
--- lib/modules/home/data/repo/chat_repo.dart
+++
@@ -1,8 +0,0 @@
-import 'package:chatbot_app/core/errors/failuers.dart';
-import 'package:chatbot_app/modules/home/data/models/massage_model.dart';
-import 'package:dartz/dartz.dart';
-
-abstract class ChatRepo {
-  Future<Either<Failure, List<MassageModel>>> loadHistory();
-  Future<Either<Failure, List<MassageModel>>> sendMessage(String text);
-}
