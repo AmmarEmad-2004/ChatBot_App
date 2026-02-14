ddbcd11ecdb5afc23c0c0c61dea143b3336ebea0
Ammar Emad <ammarzewain99@gmail.com>
2026-02-14 16:16:25 +0200

    update main

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -1,5 +1,4 @@
 const String onboardingText =
     "Using this software,you can ask you\n questions and receive articles using\n artificial intelligence assistant";
 const String apiKey = "AIzaSyD2WUOeDyxAjHilHXPkG6W3dHV9pYP4OWc";
-const String baseUrl =
-    "https://generativelanguage.googleapis.com/v1beta/models/gemini-3-flash-preview:generateContent";
+
--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,10 @@ import 'package:chatbot_app/chat_bot.dart';
 import 'package:chatbot_app/firebase_options.dart';
 import 'package:device_preview/device_preview.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-void main() async{
-   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  runApp(
-    DevicePreview(enabled: !kReleaseMode, builder: (context) => ChatBot()),
-  );
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(DevicePreview(enabled: false, builder: (context) => ChatBot()));
 }
