eb2ce29ccb5bce5703b53e5b2bacdb99f3a3d086
Ammar Emad <za0663008@gmail.com>
2026-02-15 18:01:07 +0200

    feat: Implement initial chatbot feature with Cubit state management, UI components, and a Bloc observer.

---
+++ lib/modules/chat/presentation/screens/widgets/chat_screen_massages_consumer.dart
@@ -0,0 +1,31 @@
+import 'package:chatbot_app/modules/chat/data/models/chat_massage_model.dart';
+import 'package:chatbot_app/modules/chat/presentation/logic/chat_cubit.dart';
+import 'package:chatbot_app/modules/chat/presentation/logic/chat_state.dart';
+import 'package:chatbot_app/modules/chat/presentation/screens/widgets/chat_screen_massages.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class ChatScreenMassagesConsumer extends StatelessWidget {
+  const ChatScreenMassagesConsumer({
+    super.key,
+    required this.messages,
+    required this.isTyping,
+  });
+
+  final List<ChatMassageModel> messages;
+  final bool isTyping;
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocConsumer<ChatCubit, ChatState>(
+      listener: (context, state) {
+        if (state is ChatSuccess) {
+          messages.add(state.massage);
+        }
+      },
+      builder: (context, state) {
+        return ChatScreenMassages(messages: messages, isTyping: isTyping);
+      },
+    );
+  }
+}
