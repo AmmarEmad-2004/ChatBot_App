61180e8d8e43bedd6961ada42f66e59dcfbfcb3d
Ammar Emad <za0663008@gmail.com>
2026-02-15 15:51:51 +0200

    feat: Implement the core chat feature including Gemini API integration, state management, and UI components.

--- lib/modules/chat/data/services/gemini_sevice.dart
+++ lib/modules/chat/data/services/gemini_sevice.dart
@@ -1,20 +1,32 @@
+import 'package:chatbot_app/core/constants/constants.dart';
 import 'package:chatbot_app/core/networking/api_client.dart';
 import 'package:chatbot_app/modules/chat/data/models/chat_massage_model.dart';
+import 'package:dio/dio.dart';
 
 class GeminiSevice {
   final ApiClient apiClient = ApiClient(
     baseUrl: 'https://generativelanguage.googleapis.com/v1beta/models',
+    apiKey: apiKey,
   );
 
   Future<ChatMassageModel> sendChat({
     required List<ChatMassageModel> massages,
   }) async {
-    final response = await apiClient.post(
-      url: "/gemini-3-flash-preview:generateContent",
-      data: {
-        "contents": massages.map((massage) => massage.toJson()).toList(),
-      },
-    );
-    return ChatMassageModel.fromJson(response.data);
+    try {
+      final response = await apiClient.post(
+        url: "/gemini-3-flash-preview:generateContent",
+        data: {
+          "contents": massages.map((massage) => massage.toGemini()).toList(),
+        },
+      );
+      final aiText =
+          response.data["candidates"][0]["content"]["parts"][0]["text"];
+
+      return ChatMassageModel(role: "model", text: aiText);
+    } on DioException catch (e) {
+      print("STATUS: ${e.response?.statusCode}");
+      print("DATA: ${e.response?.data}");
+      rethrow;
+    }
   }
 }
