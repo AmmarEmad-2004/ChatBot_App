78aa1bb47641e82b374587f03927d0d455eba47a
Ammar Emad <za0663008@gmail.com>
2026-02-17 23:07:00 +0200

    feat: Add ChatScreenMassages widget to display chat messages and an AI typing indicator.

--- lib/modules/chat/presentation/screens/widgets/chat_screen_massages.dart
+++ lib/modules/chat/presentation/screens/widgets/chat_screen_massages.dart
@@ -1,7 +1,6 @@
 import 'package:chatbot_app/modules/chat/data/models/chat_massage_model.dart';
 import 'package:chatbot_app/modules/chat/presentation/screens/widgets/ai_typing.dart';
 import 'package:chatbot_app/modules/chat/presentation/screens/widgets/chat_bubble.dart';
-import 'package:chatbot_app/modules/chat/presentation/screens/widgets/error_massage.dart';
 import 'package:flutter/material.dart';
 
 class ChatScreenMassages extends StatelessWidget {
@@ -9,14 +8,10 @@ class ChatScreenMassages extends StatelessWidget {
     required this.messages,
     super.key,
     required this.isTyping,
-    this.hasError = false,
-    this.onRetry,
   });
 
   final List<ChatMassageModel> messages;
   final bool isTyping;
-  final bool hasError;
-  final VoidCallback? onRetry;
 
   @override
   Widget build(BuildContext context) {
@@ -24,12 +19,9 @@ class ChatScreenMassages extends StatelessWidget {
       padding: const EdgeInsets.only(bottom: 70),
       child: ListView.builder(
         padding: const EdgeInsets.all(8),
-        itemCount: messages.length + (isTyping ? 1 : 0) + (hasError ? 1 : 0),
+        itemCount: messages.length + (isTyping ? 1 : 0),
         itemBuilder: (context, index) {
-          if (hasError && index == messages.length) {
-            return ErrorMassage(onRetry: onRetry ?? () {});
-          } else if (isTyping &&
-              index == messages.length + (hasError ? 1 : 0)) {
+          if (isTyping && index == messages.length) {
             return const AiTyping();
           } else {
             return ChatBubble(message: messages[index]);
