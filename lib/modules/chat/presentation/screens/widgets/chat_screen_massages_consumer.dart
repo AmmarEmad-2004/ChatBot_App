f0a706e8187e151a85ba772f98a206e660c65114
Ammar Emad <za0663008@gmail.com>
2026-02-17 23:13:32 +0200

    feat: Add `ChatScreenMassagesConsumer` widget to display chat messages or a welcome screen based on chat state.

--- lib/modules/chat/presentation/screens/widgets/chat_screen_massages_consumer.dart
+++ lib/modules/chat/presentation/screens/widgets/chat_screen_massages_consumer.dart
@@ -6,57 +6,26 @@ import 'package:chatbot_app/modules/chat/presentation/screens/widgets/chat_scree
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class ChatScreenMassagesConsumer extends StatefulWidget {
+class ChatScreenMassagesConsumer extends StatelessWidget {
   const ChatScreenMassagesConsumer({super.key, required this.messages});
 
   final List<ChatMassageModel> messages;
-
-  @override
-  State<ChatScreenMassagesConsumer> createState() =>
-      _ChatScreenMassagesConsumerState();
-}
-
-class _ChatScreenMassagesConsumerState
-    extends State<ChatScreenMassagesConsumer> {
-  bool hasError = false;
-  String? errorMessage;
 
   @override
   Widget build(BuildContext context) {
     return BlocConsumer<ChatCubit, ChatState>(
       listener: (context, state) {
         if (state is ChatSuccess) {
-          setState(() {
-            hasError = false;
-            errorMessage = null;
-          });
-          widget.messages.add(state.massage);
-        } else if (state is ChatFailuer) {
-          setState(() {
-            hasError = true;
-            errorMessage = state.message;
-          });
+          messages.add(state.massage);
         }
       },
 
       builder: (context, state) {
-        return widget.messages.isEmpty
+        return messages.isEmpty
             ? ChatScreenWelcome()
             : ChatScreenMassages(
-                messages: widget.messages,
+                messages: messages,
                 isTyping: state is ChatLoading,
-                hasError: hasError,
-                onRetry: () {
-                  setState(() {
-                    hasError = false;
-                    errorMessage = null;
-                  });
-                  if (widget.messages.isNotEmpty) {
-                    context.read<ChatCubit>().sendMessage(
-                      newMessages: widget.messages,
-                    );
-                  }
-                },
               );
       },
     );
