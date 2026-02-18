d77a1c2b15bba298612bfaba523943383d1409c9
Ammar Emad <za0663008@gmail.com>
2026-02-18 22:59:40 +0200

    feat: Create ChatScreenMassages widget to render chat messages, AI typing indicator, and failure messages.

--- lib/modules/chat/presentation/screens/widgets/chat_screen_massages.dart
+++ lib/modules/chat/presentation/screens/widgets/chat_screen_massages.dart
@@ -1,17 +1,19 @@
 import 'package:chatbot_app/modules/chat/data/models/chat_massage_model.dart';
 import 'package:chatbot_app/modules/chat/presentation/screens/widgets/ai_typing.dart';
 import 'package:chatbot_app/modules/chat/presentation/screens/widgets/chat_bubble.dart';
+import 'package:chatbot_app/modules/chat/presentation/screens/widgets/error_massage.dart';
 import 'package:flutter/material.dart';
 
 class ChatScreenMassages extends StatelessWidget {
   const ChatScreenMassages({
     required this.messages,
     super.key,
-    required this.isTyping,
+    required this.isTyping, required this.isfailuer,
   });
 
   final List<ChatMassageModel> messages;
   final bool isTyping;
+  final bool isfailuer;
 
   @override
   Widget build(BuildContext context) {
@@ -22,12 +24,16 @@ class ChatScreenMassages extends StatelessWidget {
         padding: const EdgeInsets.all(8),
         itemCount: messages.length + (isTyping ? 1 : 0),
         itemBuilder: (context, index) {
-          var newIndex = messages.length - (index + (isTyping ? 0 : 1));
+          var newIndex = messages.length - (index + (isTyping || isfailuer ? 0 : 1));
           // or
           // var reverseList = messages.reversed.toList();
           if (isTyping && index == 0) {
             return const AiTyping();
-          } else {
+          } 
+          if(isfailuer && index == 0){
+            return ErrorMassage(onRetry: () {  },);
+          }
+          else {
             return ChatBubble(message: messages[newIndex]);
           }
         },
