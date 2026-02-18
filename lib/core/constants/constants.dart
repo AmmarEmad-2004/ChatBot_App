b23361892e528967b4efbb41d263dfb49e0d67f2
Ammar Emad <za0663008@gmail.com>
2026-02-19 01:26:41 +0200

    feat: Add `ChatScreenMassages` widget for displaying chat messages with AI typing and error handling, and introduce `constants.dart` for onboarding text and API key.

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -1,3 +1,3 @@
 const String onboardingText =
     "Using this software,you can ask you\n questions and receive articles using\n artificial intelligence assistant";
-const String apiKey = "AIzaSyCyZWD5v58J8ncb2trzGUPy_f_-WJdnQl";
+const String apiKey = "AIzaSyC0aWf01J8fBQOomf9mAhMrFmuZ9z6TDGcc";
