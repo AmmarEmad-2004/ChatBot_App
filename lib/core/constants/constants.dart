c8636d7d1fc6435d697942b07b29a987b7729e3f
Ammar Emad <za0663008@gmail.com>
2026-02-18 22:30:14 +0200

    feat: Add onboarding text and API key constants.

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -1,3 +1,3 @@
 const String onboardingText =
     "Using this software,you can ask you\n questions and receive articles using\n artificial intelligence assistant";
-const String apiKey = "AIzaSyBEEFHiKgbgBE4zFYbA8ldeRqtKL3H3Jz8";
+const String apiKey = "AIzaSyCyZWD5v58J8ncb2trzGUPy_f_-WJdnQlU";
