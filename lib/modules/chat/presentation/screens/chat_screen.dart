2bf1d46d4b22c44843eedc363f9aa0b3a934979c
Ammar_Emad <za0663008@gmail.com>
2026-02-27 05:57:32 +0200

    feat: Implement initial chatbot feature with GetIt for dependency injection, API client, Gemini service, and chat cubit.

--- lib/modules/chat/presentation/screens/chat_screen.dart
+++ lib/modules/chat/presentation/screens/chat_screen.dart
@@ -1,5 +1,4 @@
-import 'package:chatbot_app/modules/chat/data/services/gemini_sevice.dart';
-import 'package:chatbot_app/modules/chat/data/repos/gemini_chat_repo_imple.dart';
+import 'package:chatbot_app/core/utils/setup_get_it.dart';
 import 'package:chatbot_app/modules/chat/presentation/logic/chat_cubit.dart';
 import 'package:chatbot_app/modules/chat/presentation/screens/widgets/chat_screen_body.dart';
 import 'package:flutter/material.dart';
@@ -14,7 +13,7 @@ class ChatScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.white,
       body: BlocProvider(
-        create: (_) => ChatCubit(GeminiChatRepoImple(geminiSevice: GeminiSevice())),
+        create: (_) => getIt<ChatCubit>(),
         child: ChatScreenBody(),        
       ),
     );
