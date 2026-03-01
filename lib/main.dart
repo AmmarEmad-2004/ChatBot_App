05bb919df44b05acde158ca5abd2710a6f6cfdb3
Ammar_Emad <za0663008@gmail.com>
2026-03-01 05:13:35 +0200

    feat: Implement initial application setup with DevicePreview and BlocObserver.

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,11 @@
 import 'package:chatbot_app/chat_bot.dart';
 import 'package:chatbot_app/core/helpers/bloc_observar.dart';
-import 'package:chatbot_app/core/utils/setup_get_it.dart';
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  setupGetIt();
   Bloc.observer = AppBlocObserver();
   runApp(DevicePreview(enabled: true, builder: (context) => ChatBot()));
 }
