44e8cf8bad39d78c7c26bc7151fba87da911550d
Ammar_Emad <za0663008@gmail.com>
2026-03-28 23:24:19 +0200

    feat: implement dependency injection with GetIt and integrate ChatCubit into ChatScreen

---
+++ lib/core/utils/setup_get_it.dart
@@ -0,0 +1,18 @@
+import 'package:chatbot_app/core/networking/api_client.dart';
+import 'package:chatbot_app/modules/chat/data/repos/chat_repo.dart';
+import 'package:chatbot_app/modules/chat/data/repos/gemini_chat_repo_imple.dart';
+import 'package:chatbot_app/modules/chat/data/services/gemini_sevice.dart';
+import 'package:chatbot_app/modules/chat/presentation/logic/chat_cubit.dart';
+import 'package:get_it/get_it.dart';
+
+var getIt = GetIt.instance;
+
+
+void setupGetIt(){
+  getIt.registerSingleton<ApiClient>(ApiClient());
+  getIt.registerLazySingleton<GeminiSevice>(() => GeminiSevice(apiClient: getIt<ApiClient>()));
+  getIt.registerLazySingleton<ChatRepo>(() => GeminiChatRepoImple(geminiSevice: getIt<GeminiSevice>()));
+
+
+  getIt.registerFactory<ChatCubit>(() => ChatCubit(repo: getIt<ChatRepo>()));
+}
